f80c20ff3efecbfd111d133738cdf02d74dbcc5a
Karam Taha <karamtaha15@gmail.com>
2022-12-06 11:37:56 +0200

    initial commit

---
+++ React/first_app/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {    
+    render() {
+      return (
+        <>
+          <h1>Hello Dojo!</h1>
+          <h3>Things I need to do:</h3>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt.Everest</li>
+            <li>Run a Marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+        </>
+      ); 
+    }
+  }
+
+export default App;
