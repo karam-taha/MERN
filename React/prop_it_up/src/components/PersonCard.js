9d6a01deb0ecdc171ea829449c4613c4ec2a6c1a
Karam Taha <karamtaha15@gmail.com>
2022-12-06 16:11:33 +0200

    added button to increment age

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            addAge: this.props.age,
+            show:true
+        };
+    }
     render(){
         return (
         <div>
             <h1>{this.props.firstname}, {this.props.lastname}</h1>
-            <p>Age: {this.props.age}</p>
+            <p>Age: {this.state.addAge}</p>
             <p>Hair Color: {this.props.haircolor}</p>
+            <button onClick={ () => { this.setState({ addAge: this.state.addAge + 1 }) } }>Birthday button for {this.props.firstname} {this.props.lastname}</button>
         </div>
         );
     }
